9b112d58d106abb881cc61b8da9829a9dbf5b39e
cvincent <cedric.vincent@eurisis.fr>
2020-06-23 12:34:24 +0200

    Test

--- src/rabbit_exchange_type_tlvc.erl
+++ src/rabbit_exchange_type_tlvc.erl
@@ -1,6 +1,5 @@
 -module(rabbit_exchange_type_tlvc).
 -include_lib("rabbit_common/include/rabbit.hrl").
--include_lib("rabbit/include/amqqueue.hrl").
 -include("rabbit_tlvc_plugin.hrl").
 
 -behaviour(rabbit_exchange_type).
