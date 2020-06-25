885cf9fa1f2a1a6143779c7a4ca36498c1720204
cvincent <cedric.vincent@eurisis.fr>
2020-06-25 13:32:51 +0200

    Test compilation

--- src/rabbit_exchange_type_tlvc.erl
+++ src/rabbit_exchange_type_tlvc.erl
@@ -4,3 +4,9 @@
 -include("rabbit_tlvc_plugin.hrl").
 
 -behaviour(rabbit_exchange_type).
+
+-export([description/0, serialise_events/0, route/2]).
+-export([validate/1, validate_binding/2,
+         create/2, recover/2, delete/3, policy_changed/2,
+         add_binding/3, remove_bindings/3, assert_args_equivalence/2]).
+-export([info/1,info/2]).
